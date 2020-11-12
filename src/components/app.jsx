01d71010cfe4c7d8e7f96907a4f0e9185911d318
Phaedon Valasiadis <faival@protonmail.com>
2020-11-12 11:52:18 +0100

    adding giphy api..

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import giphy from 'giphy';
 
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "J2runRfQPvK3zqSNX5"
+    }
+  }
+
+  search = (query) => {
+    giphy('uXy74uXuvP1WtYYE4f6CdMcZRA9Dq4d1').search({
+      q: query,
+      rating: 'g'
+    }, function (error, result) {
+      console.log(result);
+    // Res contains gif data!
+});
+  }
+
   render() {
-    const gifs = [
-      { id: "J2runRfQPvK3zqSNX5"},
-      { id: "YplU0FU3XHwaMmA6HM"}
-    ];
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="J2runRfQPvK3zqSNX5"/>
+            <Gif id={this.state.selectedGifId}/>
           </div>
         </div>
         <div className="right-scene">
-        <GifList gifs={gifs} />
+        <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
